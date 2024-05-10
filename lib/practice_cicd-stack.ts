552146b1a1727fcc9d1dfbd117f2623e65d3f794
praveenkilhore1 <praveen.kilhore@mavenwave.com>
2024-05-10 11:08:22 +0530

    cdk app - first

--- lib/practice_cicd-stack.ts
+++ lib/practice_cicd-stack.ts
@@ -1,16 +1,21 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class PracticeCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'CICDVPC', {
+      maxAzs: 3,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'PracticeCicdQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const instance = new ec2.Instance(this, 'CICDInstance', {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+      machineImage: ec2.MachineImage.latestAmazonLinux2023()
+    });
   }
 }
