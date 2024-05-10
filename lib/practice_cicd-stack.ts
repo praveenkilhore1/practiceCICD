23b0a368c3ed291123fe1998a22ac12004255ff1
praveenkilhore1 <praveen.kilhore@mavenwave.com>
2024-05-10 12:44:40 +0530

    image changed

--- lib/practice_cicd-stack.ts
+++ lib/practice_cicd-stack.ts
@@ -15,7 +15,7 @@ export class PracticeCicdStack extends cdk.Stack {
     const instance = new ec2.Instance(this, 'CICDInstance', {
       vpc,
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
-      machineImage: ec2.MachineImage.latestAmazonLinux2023()
+      machineImage: ec2.MachineImage.latestAmazonLinux2()
     });
   }
 }
