498985bd8080363b50a71592bb06ab9b9d5f31aa
praveenkilhore1 <praveen.kilhore@mavenwave.com>
2024-05-10 14:02:19 +0530

    bucket added

--- lib/practice_cicd-stack.ts
+++ lib/practice_cicd-stack.ts
@@ -2,15 +2,27 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import {
+  RemovalPolicy,
+  aws_s3 as S3,
+} from 'aws-cdk-lib';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class PracticeCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const vpc = new ec2.Vpc(this, 'CICDVPC', {
-      maxAzs: 2,
+    const bucket = new S3.Bucket(this, 'CICDBucket', {
+      bucketName: 'CICDBucket',
+      blockPublicAccess: S3.BlockPublicAccess.BLOCK_ALL,
+      publicReadAccess: false,
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
+
+    // const vpc = new ec2.Vpc(this, 'CICDVPC', {
+    //   maxAzs: 2,
+    // });
 
     // const instance = new ec2.Instance(this, 'CICDInstance', {
     //   vpc,
